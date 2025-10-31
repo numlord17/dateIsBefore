0867d2af509b38f593c902d9d8f121eb953e2ccb
numlord17 <numlord17@gmail.com>
2025-10-31 09:50:16 -0700

    Update Grader.java

--- Grader.java
+++ Grader.java
@@ -1,8 +1,33 @@
+import java.util.Scanner;
 public class Grader
    {
       public static void main(String[] args)
       {
+         Scanner scanner = new Scanner(System.in);
+         System.out.println("What is your GitHub Username?");
+         String githubUsername = scanner.nextLine();
+         System.out.println("What is your shoe color?");
+         String shoeColor = scanner.nextLine();
+         System.out.println(Grade(githubUsername, shoeColor));
+         
+      }
       
+      public static int Grade(String name,String color)
+      {
+         int points = 0;
+         String firstChar = name.charAt(0)+"";
+         String firstCharUpper = firstChar.toUpperCase();
+         if ((firstCharUpper == "A" || firstCharUpper == "E" || firstCharUpper == "I" || firstCharUpper == "O" || firstCharUpper == "U") && (color != "white"))
+         {
+            points += 50;
+         }
+         int asciiValue = (int)(name.charAt(name.length() - 1));
+         System.out.println(asciiValue);
+         if (asciiValue <  105 || color != "white")
+         {
+            points += 3000;
+         }
+         return points;
       }
    
    }
